bff30060c679aded706a04e649a07062d5471b89
STakko <91719838+STakko@users.noreply.github.com>
2021-10-11 15:08:36 +0900

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '961662e80fcc51484f4c622dbf126da53fe79d09ddaf36733dec175ac8acf677e8dbf852ceaa3763f86955b1337428524d8b312ee0f4c2ef79e270a5253517ab'
